b913676f4949d963176c51e528576a7a4cc4429a
Madirakshi <madicubewebtech@gmail.com>
2025-10-23 10:29:23 +0530

    23-10-2025

--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -1,20 +1,14 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:school_management_frontend/main.dart';
-
+import 'package:school_management_frontend/screens/bottom_screens.dart/timetable.dart';
 import 'package:school_management_frontend/screens/bottom_screens.dart/chat.dart';
-import 'package:school_management_frontend/screens/bottom_screens.dart/home.dart';
 import 'package:school_management_frontend/screens/bottom_screens.dart/notification.dart';
 import 'package:school_management_frontend/screens/bottom_screens.dart/profile.dart';
-import 'package:school_management_frontend/screens/bottom_screens.dart/student_dashboard.dart';
-
-import 'package:school_management_frontend/screens/bottom_screens.dart/timetable.dart';
-import 'package:school_management_frontend/screens/responsive/desktop_scaffold.dart';
-import 'package:school_management_frontend/screens/responsive/mobile_scaffold.dart';
 import 'package:school_management_frontend/screens/responsive/responsive_layout.dart';
+import 'package:school_management_frontend/screens/responsive/mobile_scaffold.dart';
 import 'package:school_management_frontend/screens/responsive/tablet_scaffold.dart';
+import 'package:school_management_frontend/screens/responsive/desktop_scaffold.dart';
 import 'package:school_management_frontend/theme/app_colors.dart';
-
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class BottomNavbar extends StatefulWidget {
   const BottomNavbar({super.key});
@@ -40,37 +34,39 @@ class _BottomNavbarState extends State<BottomNavbar> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final showBottomNav = screenWidth < 1100;
     return Scaffold(
-      // Body will show selected screen
       body: SafeArea(
-        top: false, // We don't need SafeArea at top
+        top: false,
         child: IndexedStack(
           index: _selectedIndex,
           children: _screens,
         ),
       ),
-      bottomNavigationBar: SafeArea(
-        top: false, // Only bottom safe
-        child: CurvedNavigationBar(
-          backgroundColor: Colors.transparent,
-          color: AppColors.green,
-          //Colors.teal.shade200,
-          index: _selectedIndex,
-          animationDuration: const Duration(milliseconds: 300),
-          onTap: (index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          items: const [
-            Icon(Icons.schedule, color: Colors.white),
-            Icon(Icons.chat, color: Colors.white),
-            Icon(Icons.home, color: Colors.white),
-            Icon(Icons.notifications, color: Colors.white),
-            Icon(Icons.person, color: Colors.white),
-          ],
-        ),
-      ),
+      bottomNavigationBar: showBottomNav
+          ? SafeArea(
+              top: false,
+              child: CurvedNavigationBar(
+                backgroundColor: Colors.transparent,
+                color: AppColors.green,
+                index: _selectedIndex,
+                animationDuration: const Duration(milliseconds: 300),
+                onTap: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                items: const [
+                  Icon(Icons.schedule, color: Colors.white),
+                  Icon(Icons.chat, color: Colors.white),
+                  Icon(Icons.home, color: Colors.white),
+                  Icon(Icons.notifications, color: Colors.white),
+                  Icon(Icons.person, color: Colors.white),
+                ],
+              ),
+            )
+          : null,
     );
   }
 }
